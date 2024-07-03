6138b1a2902318f808f9624567b68008fa39c295
Anand <anandmaheshwari113@gmail.com>
2024-07-03 13:28:29 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import useFetch from "./useFetch.js";
+import "./styles.css";
 
-function App() {
-  return (
+export default function App(){
+  
+  const url = "https://v2.jokeapi.dev/joke/Programming?type=single";
+  const {data,loading,error,getJoke}  = useFetch(url);
+
+  if(loading){
+    <p>Loading...</p>
+  }
+
+  if(error){
+    <p>Something went wrong...</p>
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Joke Generator</h1>
+      <h2>{data.joke}</h2>
+      <button className="btn" onClick={getJoke}>New Joke</button>
     </div>
   );
+    
 }
-
-export default App;
